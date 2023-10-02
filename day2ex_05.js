e40ec16524bb4498280a2604d25935b9306a4b89
vTroubetzkoy <146069550+vTroubetzkoy@users.noreply.github.com>
2023-10-02 16:20:15 +0200

    Create day2ex_05.js

---
+++ day2ex_05.js
@@ -0,0 +1,8 @@
+const firstname="Jhon"
+const name="Doe"
+const cardCode="1111 2222 3333 4444"
+const expiryDateMonth=21
+const expiryDateYear=2042
+const computedBankInfo=firstname+","+name+","+cardCode+","+expiryDateMonth/expiryDateYear
+computedBankInfo.toString()
+displayInCard(computedBankInfo)
