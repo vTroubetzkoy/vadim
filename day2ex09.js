843b03cad9ce56459044f07d0ec92975d02619f9
vTroubetzkoy <146069550+vTroubetzkoy@users.noreply.github.com>
2023-10-02 17:42:28 +0200

    Update day2ex09.js

---
+++ day2ex09.js
@@ -0,0 +1,5 @@
+displayThisText('The variable value is ' + input_var);
+displayThisText('Its type is '+ typeof input_var);
+if(input_var === '42'|| input_var === 42){
+displayThisText(' It is meaning of life. ');
+}
