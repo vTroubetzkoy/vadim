e72c36f1168d28ecc746c688bbd56664e1dcd8ac
vTroubetzkoy <146069550+vTroubetzkoy@users.noreply.github.com>
2023-10-02 14:31:24 +0200

    Create day2 ex_02.js

---
+++ day2 ex_02.js
@@ -0,0 +1,9 @@
+let integer=true
+displayThisText(integer)
+let float="forty two"
+displayThisText(float)
+let string=42
+displayThisText(string)
+let bool=42.42
+displayThisText(bool)
+let this_is_null=null
