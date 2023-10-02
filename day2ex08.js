2ea1c51c3a44f868db390458a63fd1fde76a1230
vTroubetzkoy <146069550+vTroubetzkoy@users.noreply.github.com>
2023-10-02 17:25:49 +0200

    Create day2ex08.js

---
+++ day2ex08.js
@@ -0,0 +1,7 @@
+if (breadCount >=2 && hamCount >=1 && tunaCount >=1){
+displayThisText("I can make a royal sandwich!")}
+else if (breadCount >=2 && hamCount >=1){
+displayThisText("I can make a ham sandwich!")}
+else if (breadCount >=2 && tunaCount >=1){
+displayThisText("I can make a tuna sandwich!")}
+else {displayThisText("I'd rather be fasting tonight..")}
